eb7b6f8c031161c4a0e83af26fb5cebecd10e1f4
zielin3k <szymon.zielinski2@teb.edu.pl>
2026-04-13 13:05:26 +0200

    Add files via upload

---
+++ szkola.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2026 at 01:03 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `szkola`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `oceny`
+--
+-- Error reading structure for table szkola.oceny: #1932 - Table &#039;szkola.oceny&#039; doesn&#039;t exist in engine
+-- Error reading data for table szkola.oceny: #1064 - Something is wrong in your syntax obok &#039;FROM `szkola`.`oceny`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `przedmioty`
+--
+-- Error reading structure for table szkola.przedmioty: #1932 - Table &#039;szkola.przedmioty&#039; doesn&#039;t exist in engine
+-- Error reading data for table szkola.przedmioty: #1064 - Something is wrong in your syntax obok &#039;FROM `szkola`.`przedmioty`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uczniowie`
+--
+-- Error reading structure for table szkola.uczniowie: #1932 - Table &#039;szkola.uczniowie&#039; doesn&#039;t exist in engine
+-- Error reading data for table szkola.uczniowie: #1064 - Something is wrong in your syntax obok &#039;FROM `szkola`.`uczniowie`&#039; w linii 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
